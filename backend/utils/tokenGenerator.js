40af924ae7e56713f805c1002a3060f0db2f0da5
frenubb <143917255+frenubb@users.noreply.github.com>
2024-04-14 18:55:01 -0400

    se remueven token model y generator

--- backend/utils/tokenGenerator.js
+++
@@ -1,6 +0,0 @@
-// funcion para generar tokens temporales de 4 dígitos
-function generarToken() {
-    return Math.floor(1000 + Math.random() * 9000);
-  }
-  
-  const tokenTemporal = generarToken();  
