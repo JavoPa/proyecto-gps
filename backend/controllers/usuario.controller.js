483cd203dfe33fb17aaa39862d8d9ca38bd386c1
Cristian <115939857+k4i-0@users.noreply.github.com>
2024-04-25 00:17:26 -0400

    modificacion archivo .gitgnore

--- backend/controllers/usuario.controller.js
+++ backend/controllers/usuario.controller.js
@@ -1,112 +1,56 @@
 
 const Usuario = require('../models/usuario.model');
-const Academico = require("../models/academico.model.js");
-const Estudiante = require("../models/estudiante.model.js");
-const Funcionario = require("../models/funcionario.model.js");
-const Administrador = require("../models/administrador.model.js");
-const Guardia = require("../models/guardia.model.js");
+const {
+    crearEstudiante,
+    crearAcademico,
+    crearAdministrador,
+    crearFuncionario,
+    crearGuardia
+} = require('../services/usuario.service')
 
 
 async function crearUsuario(req,res) {
     try {
         const { body } = req;
+        //validar campo Tipo
+        // {tipo: ""}
+        if(body.tipo == null) return res.status(400).json({message: "El campo tipo es obligatorio"});
+
+        //validar campos obligatorios
+
         //validar si el usuario ya existe
         const usuario = await Usuario.findOne({rut: body.rut});
         if(usuario) return res.status(400).json({message: "El usuario ya existe"});
 
-        //validar campos obligatorios
-
+        
         //validar tipo de usuario a crear
         if(body.tipo == "Estudiante"){
-            //validar formato de los campos no obligatorios
+            const nuevoEstudiante = crearEstudiante(body);
 
-            //crear Estudiante
-            const usuarioNuevo = new Estudiante({
-                rut: body.rut,
-                nombre: body.nombre,
-                apellido: body.apellido,
-                fono: body.fono,
-                correo: body.correo,
-                password: body.password,
-                rol: body.rol,
-                carrera: body.carrera,
-                situacion_academica: body.situacion_academica
-            }).save();
-            return res.status(200).json({message: "Usuario creado correctamente"}, usuarioNuevo);
+            return res.status(200).json({message: "Usuario creado correctamente"}, nuevoEstudiante);
         }
 
         if(body.tipo == "Guardia"){
-            //validar formato de los campos no obligatorios
-
-            const usuarioNuevo = new Guardia({
-                rut: body.rut,
-                nombre: body.nombre,
-                apellido: body.apellido,
-                fono: body.fono,
-                correo: body.correo,
-                password: body.password,
-                rol: body.rol,
-                cargo: body.cargo,
-                situacion_laboral: body.situacion_laboral
-            }).save();
-            return res.status(200).json({message: "Usuario creado correctamente"}, usuarioNuevo);
+            const nuevoGuardia = crearGuardia(body);
+            return res.status(200).json({message: "Usuario creado correctamente"}, nuevoGuardia);
         }
 
         if(body.tipo == "Academico"){
-            //validar formato de los campos no obligatorios
-
-            const usuarioNuevo = new Academico({
-                rut: body.rut,
-                nombre: body.nombre,
-                apellido: body.apellido,
-                fono: body.fono,
-                correo: body.correo,
-                password: body.password,
-                rol: body.rol,
-                cargo: body.cargo,
-                area: body.area,
-                situacion_laboral: body.situacion_laboral
-            }).save();
-            return res.status(200).json({message: "Usuario creado correctamente"}, usuarioNuevo);
+            const nuevoAcademico = crearAcademico(body);
+            return res.status(200).json({message: "Usuario creado correctamente"}, nuevoAcademico);
         }
 
         if(body.tipo == "Funcionario"){
-            //validar formato de los campos no obligatorios
-
-            const usuarioNuevo = new Funcionario({
-                rut: body.rut,
-                nombre: body.nombre,
-                apellido: body.apellido,
-                fono: body.fono,
-                correo: body.correo,
-                password: body.password,
-                rol: body.rol,
-                situacion_laboral: body.situacion_laboral,
-                departamento: body.departamento,
-                cargo: body.cargo
-            }).save();
-            return res.status(200).json({message: "Usuario creado correctamente"}, usuarioNuevo);
+            const nuevoFuncionario = crearFuncionario(body);
+            return res.status(200).json({message: "Usuario creado correctamente"}, nuevoFuncionario);
         }
 
         if(body.tipo == "Administrador"){
-            //validar formato de los campos no obligatorios
-
-
-            const usuarioNuevo = new Administrador({
-                rut: body.rut,
-                nombre: body.nombre,
-                apellido: body.apellido,
-                fono: body.fono,
-                correo: body.correo,
-                password: body.password,
-                rol: body.rol,
-                cargo: body.cargo,
-                departamento: body.departamento
-            }).save();
-            return res.status(200).json({message: "Usuario creado correctamente"}, usuarioNuevo);
+            const nuevoAdministrador = crearAdministrador(body);
+            return res.status(200).json({message: "Usuario creado correctamente"}, nuevoAdministrador);
         }
 
-        return res.status(400).json({message: "Tipo de usuario no encontrado"});
+        return res.status(400).json({message: "Tipo de usuario no encontrado, verifique los datos ingresados"});
         
     } catch (error) {
         console.log(error);
