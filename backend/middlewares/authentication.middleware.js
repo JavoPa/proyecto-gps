46ff2ac75bab8367376c59f7ec1f580a37329c81
Javier Placencia <javix.plax@gmail.com>
2024-05-20 18:13:15 -0400

    Se incluye el id del usuario en middleware y se actualiza env

--- backend/middlewares/authentication.middleware.js
+++ backend/middlewares/authentication.middleware.js
@@ -26,7 +26,8 @@ const verifyJWT = (req, res, next) => {
   
       jwt.verify(token, ACCESS_JWT_SECRET, (err, decoded) => {
         if (err) return respondError(req, res, 403, "No autorizado", err.message);
-        req.email = decoded.email;
+        req.id = decoded.id;
+        req.correo = decoded.correo;
         req.roles = decoded.roles;
         next();
       });
