0af1e6065a1bba99e176b66e4486fff109f2af16
MJaraSM <matias.jara2001@alumnos.ubiobio.cl>
2024-07-10 22:48:39 -0400

    Backend Gestion e Historial

---
+++ backend/controllers/historial.controller.js
@@ -0,0 +1,16 @@
+const accesoService = require("../services/historial.service");
+
+const getHistorialByUsuarioId = async (req, res) => {
+    const usuarioId = req.params.usuarioId;
+
+    try {
+        const historial = await accesoService.getHistorialByUsuarioId(usuarioId);
+        res.status(200).json(historial);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    getHistorialByUsuarioId,
+};
