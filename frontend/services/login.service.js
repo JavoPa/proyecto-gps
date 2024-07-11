bfe26c90d95ba6a66b1308342ee62c5bf8c32c5a
Javier Placencia <javix.plax@gmail.com>
2024-07-10 22:07:02 -0400

    Detalle ruta post login

--- frontend/services/login.service.js
+++ frontend/services/login.service.js
@@ -3,7 +3,7 @@ const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:5000/api';
 
 export async function Login(data) {
     try {
-        const res = await axios.post(API_URL, data );
+        const res = await axios.post(`${API_URL}/auth/login`, data );
         return res.data.data;
     } catch (error) {
         //alert('Error al iniciar sesión, vulva a intentarlo');
