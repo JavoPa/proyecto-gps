c5439f090549328323df1c400a21a46ddacf3ece
Javier Placencia <javix.plax@gmail.com>
2024-07-10 22:02:49 -0400

    Utilizacion de .env api url en frontend

--- frontend/services/login.service.js
+++ frontend/services/login.service.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import API_URL from '../constants/constantes';
+const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:5000/api';
 
 export async function Login(data) {
     try {
