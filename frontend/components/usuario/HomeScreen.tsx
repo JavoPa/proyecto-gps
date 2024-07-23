591786f7d8b8986eab6777de06dceb19a5f9ac42
Javier Placencia <javix.plax@gmail.com>
2024-07-23 16:41:38 -0400

    Detalles de acceso al bicicletero

---
+++ frontend/components/usuario/HomeScreen.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { MonoText } from '../StyledText';
+import { Text, View } from '../Themed';
+import { getAcceso } from '@/services/acceso.service';
+import Colors from '@/constants/Colors';
+
+export default function HomeScreen() {
+  const [estado, setEstado] = useState<string | null>(null);
+  useEffect(() => {
+    getAcceso().then((response) => {
+      if(response.state === "Success") {
+        setEstado(response.data);
+      }else{
+        setEstado('Hubo un problema al obtener tu estado');
+      }
+    });
+  }, []);
+  return (
+    <View>
+      <View style={styles.getStartedContainer}>
+        <Text
+          style={styles.getStartedText}
+          lightColor="rgba(0,0,0,0.8)"
+          darkColor="rgba(255,255,255,0.8)">
+          Estado actual de tu acceso
+        </Text>
+
+        <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
+          darkColor="rgba(255,255,255,0.05)"
+          lightColor="rgba(0,0,0,0.05)">
+          <MonoText>{estado}</MonoText>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightContainer: {
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  helpContainer: {
+    marginTop: 15,
+    marginHorizontal: 20,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    textAlign: 'center',
+  },
+});
