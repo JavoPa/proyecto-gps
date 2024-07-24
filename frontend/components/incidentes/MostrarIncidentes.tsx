d038b40c0deddf7292e252af3580616978d6f338
Tomas Sepulveda <tomas.sepulveda2001@alumnos.ubiobio.cl>
2024-07-24 03:06:22 -0400

    Se cambia formato de lista de todos los incidentes

--- frontend/components/incidentes/MostrarIncidentes.tsx
+++ frontend/components/incidentes/MostrarIncidentes.tsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, FlatList, ScrollView, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Pressable, Modal, ScrollView, Button } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import { getAllIncidentes } from '@/services/incidentes.service';
-import Colors from '@/constants/Colors';
 import { formatDate } from '../../Utils';
-
-// Listo
 
 interface MostrarIncidentesProps {
     navigateTo: (component: string) => void;
@@ -23,6 +20,9 @@ const MostrarIncidentes: React.FC<MostrarIncidentesProps> = ({ navigateTo }) => 
 
     const [incidentes, setIncidentes] = useState<Incidente[]>([]);
     const [error, setError] = useState<string | null>(null);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedIncidente, setSelectedIncidente] = useState<Incidente | null>(null);
+    const [consulta, setConsulta] = useState('');
 
     useFocusEffect(
         React.useCallback(() => {
@@ -47,88 +47,116 @@ const MostrarIncidentes: React.FC<MostrarIncidentesProps> = ({ navigateTo }) => 
         }, [])
     );
 
-    const renderItem = ({ item }: { item: Incidente }) => (
-        <View style={styles.tableRow}>
-            <Text style={styles.tableCell}>{formatDate(item.fecha)}</Text>
-            <Text style={styles.tableCell}>{item.hora}</Text>
-            <Text style={styles.tableCell}>{item.lugar}</Text>
-            <Text style={styles.tableCell}>{item.tipo}</Text>
-            <Text style={styles.tableCell}>{item.descripcion}</Text>
-        </View>
-    );
+    const handleModal = (id: string) => {
+        const incidente = incidentes.find((inc) => inc.id === id);
+        setSelectedIncidente(incidente || null);
+        setModalVisible(true);
+    };
 
     return (
-        <ScrollView style={styles.container} horizontal>
-            <View style={styles.table}>
-                <View style={{ margin: 10 }}>
-                    <Button title="Volver al menú de incidentes" onPress={() => navigateTo('IncidentesMenu')} />
-                </View>
-                <View style={styles.tableHeader}>
-                    <Text style={styles.tableHeaderText}>Fecha</Text>
-                    <Text style={styles.tableHeaderText}>Hora</Text>
-                    <Text style={styles.tableHeaderText}>Lugar</Text>
-                    <Text style={styles.tableHeaderText}>Tipo</Text>
-                    <Text style={styles.tableHeaderText}>Descripción</Text>
-                </View>
+        <View style={styles.container}>
+            <Text style={styles.title}>Lista de Incidentes</Text>
+            <ScrollView>
                 {error && (
                     <View>
                         <Text style={styles.errorText}>{error}</Text>
                     </View>
                 )}
                 {!error && incidentes.length === 0 && <Text>Cargando incidentes...</Text>}
-                {!error && incidentes.length > 0 && (
-                    <FlatList
-                        data={incidentes}
-                        renderItem={renderItem}
-                        keyExtractor={(item, index) => index.toString()}
-                    />
-                )}
-            </View>
-        </ScrollView>
+                {!error && incidentes.length > 0 && incidentes.map((item, index) => (
+                    <Pressable
+                        style={styles.vistaIncidente}
+                        key={index}
+                        onPress={() => handleModal(item.id)}
+                    >
+                        <Text style={styles.incidentes}>Fecha: {formatDate(item.fecha)}</Text>
+                        <Text style={styles.incidentes}>Tipo: {item.tipo}</Text>
+                    </Pressable>
+                ))}
+            </ScrollView>
+            <Modal
+                visible={modalVisible}
+                animationType="fade"
+                onRequestClose={() => setModalVisible(false)}
+                transparent={true}
+            >
+                <View style={styles.detalle}>
+                    {selectedIncidente && (
+                        <>
+                            <Text>Fecha: {formatDate(selectedIncidente.fecha)}</Text>
+                            <Text>Hora: {selectedIncidente.hora}</Text>
+                            <Text>Lugar: {selectedIncidente.lugar}</Text>
+                            <Text>Tipo: {selectedIncidente.tipo}</Text>
+                            <Text>Descripción: {selectedIncidente.descripcion}</Text>
+                            <Button title="Cerrar" onPress={() => setModalVisible(false)} />
+                        </>
+                    )}
+                </View>
+            </Modal>
+            <Pressable style={styles.bo} onPress={() => navigateTo('IncidentesMenu')} >
+                <Text style={styles.botonVolver}>Volver al menú de incidentes</Text>
+            </Pressable>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: Colors.light.background,
         padding: 10,
+    },
+    vistaIncidente : {
+        alignContent: 'center',
+        backgroundColor: '#2e76e4',
+        padding: 10,
+        marginVertical: 8,
+        marginLeft: 20,
+        marginRight: 20,
+        borderRadius: 10,
+        elevation: 5,
+    },
+    incidentes : {
+        fontSize: 19,
+        marginVertical: 2,
+        color: 'black',
+        margin: 5,
+    },
+    detalle : {
+        backgroundColor: '#0e4baa',
+        padding: 30,
+        borderRadius: 10,
+        elevation: 5,
+        margin: '12%',
+        marginTop: '50%',
+    },
+    title : {
+        fontSize: 30,
+        marginVertical: 8,
+        color: '#000',
+        textAlign: 'center',
+        backgroundColor: '#fff',
+        borderRadius: 25,
+        width: 'auto',
+    },
+    bo: {
+        backgroundColor: '#fff',
+        padding: 10,
+        margin: 10,
+        marginLeft: 50,
+        marginRight: 50,
+        borderRadius: 10,
+        alignItems: 'center',
+        borderRightColor: '#000',
+        borderWidth: 2,
     },
     errorText: {
         color: 'red',
         textAlign: 'center',
         marginVertical: 10,
     },
-    table: {
-        minWidth: 600, // Ensure the table is wide enough
-    },
-    tableHeader: {
-        flexDirection: 'row',
-        backgroundColor: Colors.light.tint,
-        padding: 10,
-    },
-    tableHeaderText: {
-        flex: 1,
-        width: '20%',
-        fontWeight: 'bold',
-        color: 'white',
-        textAlign: 'center',
-    },
-    tableRow: {
-        flexDirection: 'row',
-        paddingVertical: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: 'gray',
-        alignItems: 'center', // Center items vertically in each row
-    },
-    tableCell: {
-        width: '20%',
-        paddingHorizontal: 5,
-        textAlign: 'center',
-        justifyContent: 'center', // Center content vertically
-        alignItems: 'center', // Center content horizontally
-        height: '100%', // Make sure the cell takes the full height of the row
-    },
+    botonVolver : {
+        fontSize: 19,
+    }
 });
 
 export default MostrarIncidentes;
