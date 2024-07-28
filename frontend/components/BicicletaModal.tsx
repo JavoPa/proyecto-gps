60d6dce2db0f3be6b47637fad90e15329b14b785
frenubb <francisco.barraza2001@alumnos.ubiobio.cl>
2024-07-28 04:37:22 -0400

    mejora estilos BicicletaModal

--- frontend/components/BicicletaModal.tsx
+++ frontend/components/BicicletaModal.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Modal, View, Text, TextInput, Button, StyleSheet } from 'react-native';
-import Colors from '@/constants/Colors';
+import { Modal, View, Text, TextInput, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import CustomButton from './customButton';
 
 interface BicicletaModalProps {
     modalVisible: boolean;
@@ -9,127 +9,130 @@ interface BicicletaModalProps {
     handleFieldChange: (field: string, value: string) => void;
     handleActualizar: () => void;
     fields: {
-      marca: string;
-      color: string;
+        marca: string;
+        color: string;
     };
-  }
+}
 
 const BicicletaModal: React.FC<BicicletaModalProps> = ({ modalVisible, setModalVisible, bicicletaNotExists, handleFieldChange, fields, handleActualizar }) => {
-  return (
-    <Modal
-        animationType="fade"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <Text style={styles.titleContainer}>{!bicicletaNotExists ? 'Actualizar datos' : 'Registrar datos'}</Text>
+    const { width } = Dimensions.get('window');
 
-            <View style={styles.inputRow}>
-              <Text style={styles.title}>Marca: </Text>
-              <TextInput 
-                style={styles.input} 
-                onChangeText={(value) => handleFieldChange('marca', value)}
-                value={fields.marca}
-                maxLength={20}
-                placeholder="Ej: Oxford"
-                placeholderTextColor='gray'
-                textAlign="left"
-              />
+    return (
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+                setModalVisible(!modalVisible);
+            }}
+        >
+            <View style={styles.modalContainer}>
+                <View style={styles.modalContent}>
+                    <Text style={styles.modalTitle}>{!bicicletaNotExists ? 'Actualizar datos' : 'Registrar datos'}</Text>
+
+                    <View style={styles.line}>
+                        <Text style={styles.txtInput}>Marca: </Text>
+                        <TextInput 
+                            style={styles.input} 
+                            onChangeText={(value) => handleFieldChange('marca', value)}
+                            value={fields.marca}
+                            maxLength={20}
+                            placeholder="Ej: Oxford"
+                            placeholderTextColor='gray'
+                            textAlign="left"
+                        />
+                    </View>
+                    <View style={styles.line}>
+                        <Text style={styles.txtInput}>Modelo: </Text>
+                        <TextInput
+                            style={styles.input} 
+                            onChangeText={(value) => handleFieldChange('color', value)}
+                            value={fields.color}
+                            maxLength={20}
+                            placeholder="Ej: Azul"
+                            placeholderTextColor='gray'
+                            textAlign="left"
+                        />
+                    </View>
+                    <View style={styles.modalButtonContainer}>
+                        <TouchableOpacity style={styles.modalButton} onPress={handleActualizar}>
+                            <Text style={styles.modalButtonText}>Guardar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.modalButton} onPress={() => setModalVisible(false)}>
+                            <Text style={styles.modalButtonText}>Cancelar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
-            <View style={styles.inputRow}>
-              <Text style={styles.title}>Modelo: </Text>
-              <TextInput
-                style={styles.input} 
-                onChangeText={(value) => handleFieldChange('color', value)}
-                value={fields.color}
-                maxLength={20}
-                placeholder="Ej: Azul"
-                placeholderTextColor='gray'
-                textAlign="left"
-              />
-            </View>
-            <View style={styles.buttonContainer}>
-              <View style={styles.button}>
-                <Button
-                  title="Guardar"
-                  onPress={handleActualizar}
-                  color='green'
-                />
-              </View>
-              <View style={styles.button}>
-                <Button
-                  title="Cancelar"
-                  onPress={() => setModalVisible(false)}
-                  color='red'
-                />
-              </View>
-            </View>
-          </View>
-        </View>
-      </Modal>
-  );
+        </Modal>
+    );
 };
 
-export default BicicletaModal;
-
+const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
-    //
     modalContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)', // Optional: This adds a semi-transparent background
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        padding: 16,
     },
     modalContent: {
-      width: '90%',
-      maxWidth: 600,
-      backgroundColor: '#EDF2F4',
-      padding: 16,
-      borderRadius: 12,
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.8,
-      shadowRadius: 4,
-      elevation: 5,
+        backgroundColor: '#EDF2F4',
+        padding: 16,
+        borderRadius: 12,
+        width: width < 600 ? '90%' : '40%',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 4,
+        elevation: 5,
     },
-    titleContainer: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      marginBottom: 16,
-      color: '#13293D',
+    modalTitle: {
+        fontSize: width < 600 ? 24 : 40,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 16,
+        color: '#13293D',
     },
-    title: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      marginBottom: 16,
-      color: '#13293D',
+    line: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+    },
+    txtInput: {
+        fontSize: width < 600 ? 16 : 25,
+        fontWeight: 'bold',
+        marginTop: 5,
     },
     input: {
-      height: 40,
-      width: 150,
-      borderColor: 'gray',
-      borderWidth: 1,
-      marginLeft: 10,
-      padding: 5,
-      color: 'white',
+        height: width < 600 ? 40 : 60,
+        width: width < 600 ? '50%' : '50%',
+        borderColor: '#ccc',
+        borderWidth: 1,
+        marginBottom: 12,
+        padding: 8,
+        borderRadius: 4,
+        backgroundColor: '#FFFFFF',
     },
-    inputRow: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginBottom: 10,
+    modalButtonContainer: {
+        marginTop: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
-    buttonContainer: {
-      flexDirection: 'row',
-      justifyContent: 'flex-start'
+    modalButton: {
+        backgroundColor: '#2A628F',
+        borderRadius: 8,
+        paddingVertical: 10,
+        paddingHorizontal: 16,
+        marginHorizontal: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    button: {
-      margin: 10, // Agrega un margen a los botones
+    modalButtonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
     },
-  });
-  
+});
+
+export default BicicletaModal;
